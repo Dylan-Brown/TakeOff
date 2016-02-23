99351abb59db5e0b7715dcca51736f2307ce13c0
ANAKA ALANKAMONY <anakagold@levyvlan502.0112.wlan.wireless-resnet.upenn.edu>
2016-02-22 19:10:12 -0500

    homepage

--- app/src/main/java/takeoff/cis350/upenn/edu/takeoff/MainActivity.java
+++ app/src/main/java/takeoff/cis350/upenn/edu/takeoff/MainActivity.java
@@ -1,52 +1,63 @@
 package takeoff.cis350.upenn.edu.takeoff;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+        import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.view.View;
+        import android.content.Intent;
+        import android.widget.AdapterView;
+        import java.util.List;
+        import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+        import android.widget.ArrayAdapter;
+        import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    //Keep the default board size as 4
+    int boardSize = 4;
+    //Extra Message
+    public final static String EXTRA_MESSAGE = "spinnerValue";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
+
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    //MY CODE
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+    public void onItemSelected(AdapterView <?> parent, View view, int position, long id) {
 
-        return super.onOptionsItemSelected(item);
+    }
+
+    public void login(View view){
+        // So Login is a default Activity. Dylan, you can make it facy IF you want
+        // I'd say leave it till the end :D
+        Intent intent = new  Intent (this, LoginActivity.class);
+        //start Activity intent
+        startActivity(intent);
+    }
+
+    public void guest (View view) {
+        // Goes to my Dashboard
+        Intent intent = new  Intent (this, SearchInfo.class);
+        //start Activity intent
+        startActivity(intent);
+    }
+
+    public void sign (View view) {
+        // DYLAN, YOU NEED TO IMPLEMENT THIS
+        // This is the sign in page
+        // Change the Activity call later :)
+        Intent intent = new  Intent (this, LoginActivity.class);
+        //start Activity intent
+        startActivity(intent);
     }
 }
