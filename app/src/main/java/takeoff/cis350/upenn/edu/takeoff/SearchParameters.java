298884f7aca5a37cc9ddd2c4d321f5f898883f22
tangson <tjason@sas.upenn.edu>
2016-03-17 19:35:37 -0400

    Updated SearchQuery

--- app/src/main/java/takeoff/cis350/upenn/edu/takeoff/SearchParameters.java
+++ app/src/main/java/takeoff/cis350/upenn/edu/takeoff/SearchParameters.java
@@ -25,13 +25,13 @@ public class SearchParameters {
     private boolean refundable;
 
 
-    public SearchParameters(){
+    public SearchParameters() {
         countries = new HashSet<>();
         cities = new HashSet<>();
     }
 
     public SearchParameters(String departureDate, String arrivalDate, double budget, String countries,
-                            String cities){
+                            String cities) {
         //Set Variables
         int[] departureArr = parseDate(departureDate);
         setDepartureDate(departureArr);
@@ -43,7 +43,7 @@ public class SearchParameters {
     }
 
     public SearchParameters(String departureDate, String arrivalDate, double budget, String countries,
-                            String cities, int connectingF, boolean refundable){
+                            String cities, int connectingF, boolean refundable) {
         this(departureDate, arrivalDate, budget, countries, cities);
 
         maxConFlight = connectingF;
@@ -51,11 +51,7 @@ public class SearchParameters {
     }
 
 
-
-
-
     /**
-     *
      * @param s - String holding all the desired set of locations
      * @return Set of strings with each location parsed
      */
@@ -63,87 +59,84 @@ public class SearchParameters {
     private Set<String> parseLocations(String s) {
         Set<String> locations = new HashSet<>();
         String[] set = s.split(",");
-        for(String x : set) {
+        for (String x : set) {
             locations.add(x);
         }
         return locations;
     }
 
     /**
-     *
      * @param date - the string represent of the desired date in the format MM-dd-yyyy
      * @return integer array with the integers where [0] = month, [1] = day of month, [2] = year
      */
-    private int[] parseDate(String date){
+    private int[] parseDate(String date) {
         int[] dateArr = new int[3];
         String[] dateStrings = date.trim().split("-");
-        for(int i = 0; i < 3; i++) {
+        for (int i = 0; i < 3; i++) {
             dateArr[i] = Integer.parseInt(dateStrings[i]);
         }
         return dateArr;
     }
 
     /**
-     *
      * @param dateArr - integer array holding the values of the departure date
-     * Description: Sets the correct global variables for the departure date
+     *                Description: Sets the correct global variables for the departure date
      */
-    private void setDepartureDate (int[] dateArr){
+    private void setDepartureDate(int[] dateArr) {
         departureMonth = dateArr[0];
         departureDay = dateArr[1];
         departureYear = dateArr[2];
     }
 
     /**
-     *
      * @param date - String representation of the new departure date
-     * Description - allows future changes to the departure date
+     *             Description - allows future changes to the departure date
      */
-    protected void changeDepartureDate (String date) {
+    protected void changeDepartureDate(String date) {
         int[] departureArr = parseDate(date);
         setDepartureDate(departureArr);
     }
 
     /**
-     *
      * @param dateArr - integer array holding the values of the arrival date
-     * Description: Sets the correct global variables for the arrival date
+     *                Description: Sets the correct global variables for the arrival date
      */
-    private void setArrivalDate (int[] dateArr){
+    private void setArrivalDate(int[] dateArr) {
         arrivalMonth = dateArr[0];
         arrivalDay = dateArr[1];
         arrivalYear = dateArr[2];
     }
+
     /**
-     *
      * @param date - String representation of the new arrival date
-     * Description - allows future changes to the arrival date
+     *             Description - allows future changes to the arrival date
      */
-    protected void changeArrivalDate (String date) {
+    protected void changeArrivalDate(String date) {
         int[] departureArr = parseDate(date);
         setDepartureDate(departureArr);
     }
 
     /**
-     *
      * @param r - the boolean flag for refundable tickets
-     * Description - allows future changes to the refundable tag
+     *          Description - allows future changes to the refundable tag
      */
-    protected void changeRefundable (boolean r) {this.refundable = r;}
+    protected void changeRefundable(boolean r) {
+        this.refundable = r;
+    }
 
     /**
-     *
      * @param n - the boolean flag for maximum desired connecting flights
-     * Description - allows future changes to the desired connecting flights
+     *          Description - allows future changes to the desired connecting flights
      */
-     protected void changeCFlight (int n) {maxConFlight = n;}
+    protected void changeCFlight(int n) {
+        maxConFlight = n;
+    }
 
     /**
-     *
      * @param b - the boolean flag for maximum budget
-     * Description - allows future changes to the maximum budget
+     *          Description - allows future changes to the maximum budget
      */
-    protected  void changeBudget (double b) {
+    protected void changeBudget(double b) {
         maxBudget = b;
     }
 
@@ -153,6 +146,7 @@ public class SearchParameters {
     protected String getDepartureDate() {
         return departureMonth + "-" + departureDay + "-" + departureYear;
     }
+
     protected String getArrivalDate() {
         return arrivalMonth + "-" + arrivalDay + "-" + arrivalYear;
     }
@@ -160,9 +154,11 @@ public class SearchParameters {
     protected int getDepartureMonth() {
         return departureMonth;
     }
+
     protected int getDepartureDay() {
         return departureDay;
     }
+
     protected int getDepartureYear() {
         return departureYear;
     }
@@ -170,9 +166,11 @@ public class SearchParameters {
     protected int getArrivalMonth() {
         return arrivalMonth;
     }
+
     protected int getArrivalDay() {
         return arrivalDay;
     }
+
     protected int getArrivalYear() {
         return arrivalYear;
     }
@@ -181,21 +179,28 @@ public class SearchParameters {
     protected boolean isRefundable() {
         return refundable;
     }
+
     protected Set<String> getCountries() {
         Set<String> c = new HashSet<>();
         c.addAll(countries);
         return c;
     }
+
     protected Set<String> getCities() {
         Set<String> c = new HashSet<>();
         c.addAll(cities);
         return c;
     }
+
     protected double getBudget() {
         return maxBudget;
     }
-    protected int getMaxConFlight() {return maxConFlight;}
+
+    protected int getMaxConFlight() {
+        return maxConFlight;
+    }
 }
+
 //radio button
 //class names -> preferred class
 //connecting flights
--- app/src/main/java/takeoff/cis350/upenn/edu/takeoff/SearchQuery.java
+++ app/src/main/java/takeoff/cis350/upenn/edu/takeoff/SearchQuery.java
@@ -3,21 +3,23 @@ package takeoff.cis350.upenn.edu.takeoff;
 /**
  * Created by tangson on 3/17/16.
  */
+
 public class SearchQuery {
-    int no_of_Slices=1;
+    int no_of_Slices=1; //number of total trips; one-way is 1 slice, roundtrip is 2 slices
     String origin="";
     String destination="";
-    String date="";
-    int adultCount=1;
-    String preferredCabin="";
-    int maxPrice=99999;
-    boolean refundability=false;
-    int numberOfSolutions=499;
-    int maxStops=5;
-    String earliestTime="";
-    String latestTime="";
-    int maxConnectionDuration=4320;
-    String alliance="";
+    String date=""; //in the format of YYYY-MM-DD
+    int adultCount=1;	//number of adults (i.e. passengers)
+
+    String preferredCabin=""; //COACH, PREMIUM_COACH, BUSINESS, FIRST
+    int maxPrice=99999; //in USD
+    boolean refundability=false; //is refundable or not
+    int numberOfSolutions=499; //maximum number of search results
+    int maxStops=5; //number of connections, direct flights = 0
+    String earliestTime=""; //in the format of HH:MM (24 hours)
+    String latestTime=""; //in the format of HH:MM (24 hours)
+    int maxConnectionDuration=4320; //maximum amount in minutes
+    String alliance=""; //STAR, ONEWORLD
 
     public SearchQuery() {
     };
