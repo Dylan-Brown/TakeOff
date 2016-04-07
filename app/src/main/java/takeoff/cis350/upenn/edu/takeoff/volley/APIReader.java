f55fd3cfffb1bf1845e63403a625b7253ec53f55
Dylan Brown <dylanbrown.illustration@gmail.com>
2016-04-06 21:10:55 -0400

    volley?

---
+++ app/src/main/java/takeoff/cis350/upenn/edu/takeoff/volley/APIReader.java
@@ -0,0 +1,25 @@
+package takeoff.cis350.upenn.edu.takeoff.volley;
+
+// import com.android.volley.toolbox.*;
+// import com.android.volley.*;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+/**
+ * Created by tangson on 3/7/16.
+ */
+
+//curl -d @request.json --header "Content-Type: application/json"
+// https://www.googleapis.com/qpxExpress/v1/trips/search?key=your_API_key_here
+
+public class APIReader {
+    // // comment
+    public void Jsonreader() {
+        try {
+            Object obj = new FileReader("Dummydata");
+        } catch (FileNotFoundException e) {
+        }
+        ;
+    }
+}
---
+++ app/src/main/java/takeoff/cis350/upenn/edu/takeoff/volley/VolleyCallback.java
@@ -0,0 +1,7 @@
+package takeoff.cis350.upenn.edu.takeoff.volley;
+
+
+public interface VolleyCallback {
+    // comment
+    void onSuccessResponse(String result);
+}
